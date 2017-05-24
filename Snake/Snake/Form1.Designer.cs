7f4223539e79d6b1322ba8c07f0060a4f92038b6
NiklasBauer51 <nbauer98@hotmail.de>
2017-05-24 09:15:18 +0200

    Feld

--- Snake/Snake/Form1.Designer.cs
+++ Snake/Snake/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 363);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(784, 662);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Snake";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
